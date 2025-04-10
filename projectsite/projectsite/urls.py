352f6b0cec9348ca82737653adc3e1e3f5b43836
Enzxco <lawrencesecjadas28@gmail.com>
2025-04-10 10:41:18 +0800

    Done College Delete  View

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
 from studentorg.views import OrgMembersList, OrgMembersCreateView, OrgMembersUpdateView, OrgMembersDeleteView
 from studentorg.views import StudentsList, StudentsCreateView, StudentsUpdateView, StudentsDeleteView
-from studentorg.views import CollegeList, CollegeCreateView
+from studentorg.views import CollegeList, CollegeCreateView, CollegeUpdateView, CollegeDeleteView
 from studentorg import views
 
 urlpatterns = [
@@ -50,4 +50,6 @@ urlpatterns = [
     # College URLs
     path('college_list', views.CollegeList.as_view(), name='college-list'),
     path('college_list/add', CollegeCreateView.as_view(), name='college-add'),
+    path('college_list/<pk>', CollegeUpdateView.as_view(), name='college-update'),
+    path('college_list/<pk>/delete', CollegeDeleteView.as_view(), name='college-delete'),
 ]
